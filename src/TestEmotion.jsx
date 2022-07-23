a9d0aec62062d4404e19aad98c537caa0e3bebb1
yong-joon-kang <kangyj2197@gmail.com>
2022-07-24 02:44:50 +0900

    emotion styled component 생성
    상세내용
    - emotion css, react, styled 라이브러리 install
    - TestEmotion.jsx 컴포넌트 생성 후 정상작동 확인

---
+++ src/TestEmotion.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  color: red;
+`;
+
+const testEmotion = () => {
+  return (
+    <div>
+      <Button>doooolphin !!!</Button>
+    </div>
+  );
+};
+
+export default testEmotion;
