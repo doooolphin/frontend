098d1ebdabafc7e66b7d4781ef2b418e74a7a0c8
ziyoonee <wldbs4282@gmail.com>
2022-10-17 02:41:05 +0900

    [MODIFY] Tab story

--- src/stories/doolphin/Tab.stories.jsx
+++ src/stories/doolphin/Tab.stories.jsx
@@ -1,34 +1,26 @@
 import CapsuleTab from '@components/Common/Tab/CapsuleTab';
-import { useState } from 'react';
 
 export default {
   title: 'Doolphin/Tab',
   component: CapsuleTab,
+  argTypes: {
+    onClick: { action: 'onClick' }
+  },
   parameters: {
     layout: 'fullscreen'
   }
 };
 
-const Template = (args) => {
-  const [categorys, setCategorys] = useState([
+const Template = (args) => <CapsuleTab {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  categorys: [
     { code: 1, name: '배달', active: true },
     { code: 2, name: '돌핀1' },
     { code: 3, name: '포장' },
     { code: 4, name: '쇼핑라이브' },
     { code: 5, name: '선물하기' },
     { code: 6, name: '전국별미' }
-  ]);
-
-  const onClick = (activeCategory) => {
-    setCategorys((prevCategorys) =>
-      prevCategorys.map((category) => {
-        category.active = category.code === activeCategory.code;
-        return category;
-      })
-    );
-  };
-  return <CapsuleTab categorys={categorys} onClick={onClick} {...args} />;
+  ]
 };
-
-export const Default = Template.bind({});
-Default.args = {};
