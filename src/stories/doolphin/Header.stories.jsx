94b6831e530cf0b13e7a4e959949ce9691b6c03d
ziyoonee <wldbs4282@gmail.com>
2022-10-19 02:40:16 +0900

    [FIX] Header story default args

--- src/stories/doolphin/Header.stories.jsx
+++ src/stories/doolphin/Header.stories.jsx
@@ -3,22 +3,19 @@ import Header from '@components/Common/Header';
 export default {
   title: 'Doolphin/Header',
   component: Header,
-  parameters: {
-    layout: 'fullscreen'
+  args: {
+    address: 'E편한세상광주역5단지'
   }
 };
 
 const Template = (args) => <Header {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {
-  address: 'E편한세상광주역5단지'
-};
 
 const MainTemplate = (args) => <Header main {...args} />;
 
 export const Main = MainTemplate.bind({});
+
 Main.args = {
-  address: 'E편한세상광주역5단지',
   hasNotice: true
 };
