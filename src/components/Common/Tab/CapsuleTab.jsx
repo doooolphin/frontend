90a3393dbc02e8e53eab0e9d8c5e1a818995b422
ziyoonee <wldbs4282@gmail.com>
2022-10-19 16:33:38 +0900

    [TEST] rollback style

--- src/components/Common/Tab/CapsuleTab.jsx
+++ src/components/Common/Tab/CapsuleTab.jsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 
 const Wrapper = styled.div`
   display: flex;
-  height: 60px;
+  height: 50px;
   width: 100%;
   overflow-x: auto;
   overflow-y: hidden;
@@ -21,7 +21,7 @@ const Capsule = styled.button`
   &:first-child {
     margin-left: 10px;
   }
-  color: ${(props) => (props.active ? '#ffffff' : '#949292')};
+  color: ${(props) => (props.active ? '#ffffff' : '#797676')};
   font-weight: ${(props) => (props.active ? 600 : 'unset')};
   background-color: ${(props) => (props.active ? '#AC4AC4' : '#ffffff')};
 `;
