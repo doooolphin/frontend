82679428f3092a4bef46568f01706111dca55dd6
kangyj2197 <kangyj2197@gmail.com>
2023-03-23 17:35:48 +0900

    [FIX] BannerSlider

--- src/components/Common/BannerSlider/BannerSlider.jsx
+++ src/components/Common/BannerSlider/BannerSlider.jsx
@@ -1,7 +1,28 @@
 import Slider from 'react-slick';
 import PropTypes from 'prop-types';
-import './slick.css';
-import './slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import styled from '@emotion/styled';
+import { useState } from 'react';
+
+const SliderNum = styled.div`
+  background: #000;
+  opacity: 50%;
+  border-radius: 10px;
+  width: 35px;
+  position: absolute;
+  top: 83px;
+  right: 5px;
+`;
+
+const SliderFont = styled.div`
+  font-size: 10px;
+  display: flex;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const BannerSlider = ({ banners, onClick }) => {
   console.log(banners);
@@ -10,20 +31,28 @@ const BannerSlider = ({ banners, onClick }) => {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    afterChange: (index) => setBannerIdx(index + 1)
   };
+
+  const [bannerIdx, setBannerIdx] = useState('1');
+
   return (
     <div>
-      <h2> Single Item</h2>
       <Slider {...settings}>
         {banners.map((banner) => (
           <>
-            <div onClick={() => onClick(banner)}>
-              <img src={banner.image}></img>
+            <div key={banner.code} onClick={() => onClick(banner)}>
+              <img src={banner.image} alt={banner.alt}></img>
             </div>
           </>
         ))}
       </Slider>
+      <SliderNum>
+        <SliderFont>
+          {bannerIdx}/{banners.length}
+        </SliderFont>
+      </SliderNum>
     </div>
   );
 };
--- src/stories/doolphin/BannerSlider.stories.jsx
+++ src/stories/doolphin/BannerSlider.stories.jsx
@@ -16,11 +16,11 @@ const Template = (args) => <BannerSlider {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   banners: [
-    { code: 1, image: 'doooolphinBanner1.png' },
-    { code: 2, image: 'doooolphinBanner2.png' },
-    { code: 3, image: 'doooolphinBanner3.png' },
-    { code: 4, image: 'doooolphinBanner4.png' },
-    { code: 5, image: 'doooolphinBanner5.png' },
-    { code: 6, image: 'doooolphinBanner6.png' }
+    { code: 1, image: 'doooolphinBanner1.png', alt: 'banner1' },
+    { code: 2, image: 'doooolphinBanner2.png', alt: 'banner2' },
+    { code: 3, image: 'doooolphinBanner3.png', alt: 'banner3' },
+    { code: 4, image: 'doooolphinBanner4.png', alt: 'banner4' },
+    { code: 5, image: 'doooolphinBanner5.png', alt: 'banner5' },
+    { code: 6, image: 'doooolphinBanner6.png', alt: 'banner6' }
   ]
 };
