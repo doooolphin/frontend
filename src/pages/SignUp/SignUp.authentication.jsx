1e84fa7763841edd5fcd13b90d8c53ee028ed7fe
kangyj2197 <kangyj2197@gmail.com>
2023-03-29 18:08:12 +0900

    [ADD] SignUp.authentication Component & router

---
+++ src/pages/SignUp/SignUp.authentication.jsx
@@ -0,0 +1,5 @@
+function SignUpAuthentication() {
+  return <div>SignUp.authentication</div>;
+}
+
+export default SignUpAuthentication;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Main from '@pages/Main';
 import SignIn from '@pages/SignIn';
+import SignUpAgreement from '@pages/SignUp/SignUp.agreement';
+import SignUpAuthentication from '@pages/SignUp/SignUp.authentication';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />}></Route>
       <Route path="/sign-in" element={<SignIn />}></Route>
+      <Route path="/sign-up-agreement" element={<SignUpAgreement />}></Route>
+      <Route path="/sign-up-authentication" element={<SignUpAuthentication />}></Route>
       <Route path="*" element={<Main />}></Route>
     </Routes>
   );
