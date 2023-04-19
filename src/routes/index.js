eea7756211149112595f2e2b6f43344ca8528aaa
kangyj2197 <kangyj2197@gmail.com>
2023-04-20 00:39:01 +0900

    [ADD] home Component

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,14 @@ import SignIn from '@pages/SignIn';
 import My from '@pages/My';
 import Agreement from '@pages/SignUp/Agreement';
 import Authentication from '@pages/SignUp/Authentication';
+import Home from '@pages/Home';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />}></Route>
       <Route path="/my" element={<My />}></Route>
+      <Route path="/home" element={<Home />}></Route>
       <Route path="/sign-in" element={<SignIn />}></Route>
       <Route path="/sign-up/agreement" element={<Agreement />}></Route>
       <Route path="/sign-up/authentication" element={<Authentication />}></Route>
