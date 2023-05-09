e59a6f833fa30723d880e044caa38e1a60e33b24
jiyoon <ziyoon.lee@krafton.com>
2023-05-09 18:27:22 +0900

    [UPDATE] cypress timeout

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('Main Page', () => {
   it('Load Page', () => {
-    cy.visit('/', { timeout: 30000 });
+    cy.visit('/', { timeout: 120000 });
     cy.title().should('eq', 'Doooolphin App !');
   });
 });
