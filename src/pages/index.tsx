910815a541cb125819f0eb9a1907fc8d9a57b864
ziyoonee <wldbs4282@gmail.com>
2023-07-25 00:52:55 +0900

    [ADD] title 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import TestEmotion from '@/TestEmotion';
 import Layout from '@components/Common/Layout';
 import MainHeader from '@components/Common/Header/MainHeader';
+import Head from 'next/head';
 
 export default function Home() {
-  return <Layout header={<MainHeader hasNotice address="It's alright~ 우리집으로 가자~" />}>메인!</Layout>;
+  return (
+    <>
+      <Head>
+        <title>Doooolphin App !</title>
+      </Head>
+      <Layout header={<MainHeader hasNotice address="It's alright~ 우리집으로 가자~" />}>메인!</Layout>
+    </>
+  );
 }
